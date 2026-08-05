3b4c974976aa0b67ad22db8727a613bc356982a9
buildthebase <micholas@buildthebase.io>
2026-08-05 10:53:43 -0400

    Load shared service pricing styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Manrope } from "next/font/google";
 import "./globals.css";
 import "./event-numberless.css";
+import "./service-pricing-panels.css";
 import { siteConfig } from "./site-config";
 
 const siteUrl = siteConfig.url;
